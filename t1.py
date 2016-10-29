23df4a5772dab9c3fec104da7614948a2c5437fe
kevinwan <354798650@qq.com>
2016-10-30 02:57:34 +0800

    for a test using python mutiline in shell

---
+++ t1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import sys,json
+from pprint import pprint
+
+j1=sys.argv[1];j2=sys.argv[2];obj1 = json.load(open(j1, 'r'));del obj1['metrics'], obj1['definitions'];obj2=json.load(open(j2, 'r'))
+print json.dumps([dict(obj1.items() + it.items()) for it in ( dict({u'fragement_id' :key}.items() + value.items()) for key, value in obj2.items())])
+
