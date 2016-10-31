f10113373df077b80fa1997bf104877819b93dc6
Kevin Wang <354798650@qq.com>
2016-11-01 03:31:59 +0800

    adjust the format

--- t1.py
+++ t1.py
@@ -3,6 +3,10 @@
 import sys,json
 from pprint import pprint
 
-j1=sys.argv[1];j2=sys.argv[2];obj1 = json.load(open(j1, 'r'));del obj1['metrics'], obj1['definitions'];obj2=json.load(open(j2, 'r'))
+j1=sys.argv[1];
+j2=sys.argv[2];
+obj1 = json.load(open(j1, 'r'));
+del obj1['metrics'], obj1['definitions'];
+obj2=json.load(open(j2, 'r'))
 print json.dumps([dict(obj1.items() + it.items()) for it in ( dict({u'fragement_id' :key}.items() + value.items()) for key, value in obj2.items())])
 
